d135c7c69637a799cd404c60cfe796fbc2a22871
Nishanth Prasath <prasath.n@northeastern.edu>
2024-04-18 19:54:40 -0400

    Added Transformer Module and Updated Data Loader to Fetch Multiple Submissions

--- safefeed/data_loaders/reddit_scrapper_v2.py
+++ safefeed/data_loaders/reddit_scrapper_v2.py
@@ -44,19 +44,35 @@ def get_openai_moderation_categories(text):
         'sexual_minors': categories.sexual_minors,
         'violence': categories.violence,
         'violence_graphic': categories.violence_graphic,
-        'flagged': response.results[0].flagged  # Add flagged attribute
+        'IS_FLAGGED': response.results[0].flagged  # Add flagged attribute
     }
     return category_dict
 
+# def extract_image_url(text):
+#     # Regular expression pattern to match complete image URLs
+#     pattern = r"https?://\S+"
+#     match = re.search(pattern, text)
+#     if match:
+#         url = match.group()
+#         # Check if the URL contains any of the specified image extensions
+#         if any(ext in url.lower() for ext in ['jpg', 'jpeg', 'png', 'gif']):
+#             return url
+#     return None
 def extract_image_url(text):
     # Regular expression pattern to match complete image URLs
-    pattern = r"https?://\S+"
+    pattern = r"https?://[^\s\[\]]+\.(?:jpg|jpeg|png|gif)(?=\s|\[|\])"
     match = re.search(pattern, text)
     if match:
         url = match.group()
-        # Check if the URL contains any of the specified image extensions
-        if any(ext in url.lower() for ext in ['jpg', 'jpeg', 'png', 'gif']):
-            return url
+        return url
+    else:
+        pattern = r"https?://\S+"
+        match = re.search(pattern, text)
+        if match:
+            url = match.group()
+            # Check if the URL contains any of the specified image extensions
+            if any(ext in url.lower() for ext in ['jpg', 'jpeg', 'png', 'gif']):
+                return url
     return None
 
 def predict_image_tags(image_url):
@@ -64,7 +80,7 @@ def predict_image_tags(image_url):
     tags = result[0]
     return tags
 
-def get_data(subreddit_name, last_trigger_timestamp):
+def get_data(subreddit_id, subreddit_name, last_trigger_timestamp):
     posts_data = []
     # Convert last_trigger_timestamp to Unix timestamp
     last_trigger_timestamp = last_trigger_timestamp.timestamp()
@@ -74,24 +90,42 @@ def get_data(subreddit_name, last_trigger_timestamp):
         if submission.created_utc > last_trigger_timestamp:
             post_text = submission.title + " " + submission.selftext
             post_data = {
-                'post_id': submission.id,
-                'title': submission.title,
-                'author': submission.author.name if submission.author else '[deleted]',
-                'text': submission.selftext,
-                'url': submission.url,
+                'SUBMISSION_ID': submission.id,
+                'SUBREDDIT_ID': subreddit_id,
+                'SUBMISSION_TITLE': submission.title,
+                'SUBMISSION_URL': submission.url,
+                'SUBMISSION_AUTHOR': submission.author.name if submission.author else '[deleted]',
+                'SUBMISSION_TIMESTAMP': submission.created_utc,
+                'SUBMISSION_TEXT': submission.selftext,
                 **get_openai_moderation_categories(post_text)  # Add OpenAI moderation categories
             }
 
             # Check if the post contains an image URL in the text
-            image_url = extract_image_url(submission.selftext)
-            if image_url:
-                image_tags = predict_image_tags(image_url)
-                post_data['image_tags'] = image_tags
-            else:
-                # If no image URL found in the text, check if the post URL is an image
-                if submission.url.endswith(('jpg', 'jpeg', 'png', 'gif')):
-                    image_tags = predict_image_tags(submission.url)
-                    post_data['image_tags'] = image_tags
+            print(submission.selftext)
+            try:
+                image_url = extract_image_url(submission.selftext)
+                if image_url:
+                    print("if", image_url)
+                    image_tags = predict_image_tags(image_url)
+                    post_data['IMAGE_CAPTION'] = image_tags
+                else:
+                    # If no image URL found in the text, check if the post URL is an image
+                    if submission.url.endswith(('jpg', 'jpeg', 'png', 'gif')):
+                        print("else", submission.url)
+                        image_tags = predict_image_tags(submission.url)
+                        post_data['IMAGE_CAPTION'] = image_tags
+            except ValueError:
+                print("ValueError occurred")
+
+
+            post_data['SENTIMENT_CATEGORY'] = 'Positive' #Need to add sentiment response from LLM through FastAPI
+            post_data['MODERATION_REASON'] = 'Why it has been moderated if moderated' #Response from LLM
+            post_data['REVIEWED'] = 'true' #if questionable from response make it false
+            post_data['IS_IMAGE_GENERAL'] = 'false' #get it from LLM for all the image values
+            post_data['IS_IMAGE_SENSITIVE'] = 'false'
+            post_data['IS_IMAGE_EXPLICIT'] = 'false'
+            post_data['IS_QUESTIONABLE'] = 'false'
+
 
             posts_data.append(post_data)
 
@@ -111,30 +145,34 @@ def update_last_trigger_timestamp(conn, subreddit_name):
 def load_data(*args, **kwargs):
     # Connect to Snowflake
     conn = snowflake.connector.connect(
-        database = '',
-        account = '',
-        user = '',
-        password = '',
-        schema=''
+        database = get_secret_value('Nishanth_SF_DB'),
+        account = get_secret_value('Nishanth_SF_ACCOUNT'),
+        user = get_secret_value('Nishanth_SF_User'),
+        password = get_secret_value('Nishanth_SF_PSWD'),
+        schema=get_secret_value('Nishanth_SF_Schema')
     )
 
     # Fetch subreddit names and last trigger timestamps from Snowflake
     cursor = conn.cursor()
-    cursor.execute("SELECT SUBREDDIT_NAME, LAST_TRIGGER_TIMESTAMP FROM REDDIT.SUBREDDIT")
+    cursor.execute("SELECT SUBREDDIT_ID, SUBREDDIT_NAME, LAST_TRIGGER_TIMESTAMP FROM REDDIT.SUBREDDIT")
     subreddits = cursor.fetchall()
 
+    all_posts = []
+
     # Loop through each subreddit
-    for subreddit_name, last_trigger_timestamp in subreddits:
+    for subreddit_id, subreddit_name, last_trigger_timestamp in subreddits:
         # Call load_data() function with the subreddit name and last trigger timestamp
-        df_posts = get_data(subreddit_name=subreddit_name, last_trigger_timestamp=last_trigger_timestamp)
-        
+        df_posts = get_data(subreddit_id=subreddit_id, subreddit_name=subreddit_name, last_trigger_timestamp=last_trigger_timestamp)
+        all_posts.append(df_posts)
         # Update LAST_TRIGGER_TIMESTAMP in Snowflake
-        update_last_trigger_timestamp(conn, subreddit_name)
+        # update_last_trigger_timestamp(conn, subreddit_name)
+
+    df_all_posts = pd.concat(all_posts, ignore_index=True)
 
     # Close connection
     conn.close()
 
-    return df_posts
+    return df_all_posts
 
 
 # Test function
---
+++ safefeed/transformers/combine_categories.py
@@ -0,0 +1,31 @@
+from pandas import DataFrame
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data: DataFrame, *args, **kwargs):
+
+    data['IS_TEXT_HATE_SPEECH'] = data['hate'] | data['hate_threatening']
+    data['IS_TEXT_HARASSMENT'] = data['harassment'] | data['harassment_threatening']
+    data['IS_TEXT_SELF_HARM'] = data['self_harm'] | data['self_harm_intent'] | data['self_harm_instructions']
+    data['IS_TEXT_SEXUAL_CONTENT'] = data['sexual'] | data['sexual_minors']
+    data['IS_TEXT_VIOLENCE'] = data['violence'] | data['violence_graphic']
+
+    # Drop the original columns
+    data.drop(['hate', 'hate_threatening', 'harassment', 'harassment_threatening',
+               'self_harm', 'self_harm_intent', 'self_harm_instructions',
+               'sexual', 'sexual_minors', 'violence', 'violence_graphic'], axis=1, inplace=True)
+
+    return data
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
+    print(output)
